bc41fe353c59c25534f14ccf68aa8a2b6a050e30
Patrick Utz <utzpatrick91@gmail.com>
2018-09-18 12:31:17 -0700

    Create new class exmaple for LinkedList of ints

---
+++ class_examples/src/int_linkedlist/IntLinkedList.java
@@ -0,0 +1,82 @@
+package int_linkedlist;
+
+public class IntLinkedList {
+	
+	private int size;
+	private Node head;
+	
+	IntLinkedList () {
+		head = null;
+		size = 0;
+	}
+	
+	public int getSize () {
+		throw new UnsupportedOperationException();
+	}
+	
+	public void prepend (int item) {
+		Node heldHead = head;
+		head = new Node(item);
+		head.next = heldHead;
+		size++;
+	}
+	
+	public Iterator getIteratorAt (int index) {
+		if (index > size || index <0) {
+			throw new IllegalArgumentException();
+		}
+		Iterator it = new Iterator();
+		while (index > 0) {
+			it.next();
+			index--;
+		}
+		return it;
+	}
+	
+	private class Node {
+		
+		int data;
+		Node next;
+		
+		Node(int d) {
+			data = d;
+			next = null;
+		}
+		
+	}
+	
+	public class Iterator {
+	
+		private Node current;
+		
+		Iterator () {
+			current = head;
+		}
+		
+		public boolean hasNext () {
+			return current != null && current.next != null;
+		}
+		
+		public void next () {
+			if (current == null) {return;}
+			current = current.next;		
+		}
+		
+		public int getCurrentInt () {
+			return current.data;
+		}
+	}
+	
+	public static void main (String[] args) {
+		IntLinkedList coolJ = new IntLinkedList();
+		coolJ.prepend(3);
+		coolJ.prepend(2);
+		coolJ.prepend(1);
+		IntLinkedList.Iterator it = coolJ.getIteratorAt(0);
+		System.out.println(it.getCurrentInt());
+		it.next();
+		System.out.println(it.getCurrentInt());
+		it.next();
+		System.out.println(it.getCurrentInt());
+	}
+}
--- class_examples/src/intlist/IntList.java
+++ class_examples/src/intlist/IntList.java
@@ -2,67 +2,92 @@ package intlist;
 
 public class IntList implements IntListInterface {
 	
-	private int[] items;
-	private int size;
-	private static final int START_SIZE = 8;
-	
-	IntList () {
-		size = 0;
-		items = new int[START_SIZE];
-	}
-	
-	public void append (int toAdd) {
-		checkAndGrow();
-		items[size] = toAdd;
-		size++;
-	}
-	
-	public void removeAt (int index) {
-		shiftLeft(index);
-		size--;
-	}
-	
-	public void insertAt (int toAdd, int index) {
-		throw new UnsupportedOperationException();
-	}
-	
-	public int getAt (int index) {
-		if (index >= size) {
-			throw new ArrayIndexOutOfBoundsException();
-		}
-		return items[index];
-	}
-	
-	private void checkAndGrow () {
-		// Case: big enough to fit another item
-		if (size < items.length) {
-			return;
-		}
-		
-		// Case: not big enough to fit another item;
-		// "grow" our items array
-		int[] newItems = new int[items.length * 2];
-		
-		// Copy old items into new
-		for (int i = 0; i < items.length; i++) {
-			newItems[i] = items[i];
-		}
-		
-		// Change the items reference to the new larger array
-		items = newItems;
-	}
-	
-	public void shiftLeft (int index) {
-		for (int i = index; i < size -1; i++) {
-			items[i] = items[i + 1];
-		}
-	}
+	// Fields
+    private int[] items;
+    private int   size;
+    private static final int START_SIZE = 8;
+    
+    // Constructor
+    IntList () {
+        items = new int[START_SIZE];
+        size  = 0;
+    }
 
-	public static void main (String[] args) {
-		IntList intyFresh = new IntList();
-		for (int i = 0; i < 10; i++) {
-			intyFresh.append(i);
-			System.out.println(intyFresh.getAt(i));
-		}
-	}
+    public int getAt(int index) {
+        indexValidityCheck(index);
+        return items[index];
+    }
+
+    public void append(int toAdd) {
+        checkAndGrow();
+        items[size] = toAdd;
+        size++;
+    }
+    
+    public void prepend (int toAdd) {
+    	throw new UnsupportedOperationException();
+    }
+
+    public void insertAt (int toAdd, int index) {
+    	checkAndGrow();
+    	
+    	for (int i = ; i < size-1; i++) {
+            items[i] = items[i+1];
+        }
+    }
+    
+    public void removeAll (int toRemove) {
+        throw new UnsupportedOperationException();
+    }
+
+    public void removeAt(int index) {
+        indexValidityCheck(index);
+        shiftLeft(index);
+        size--;
+    }
+    
+    private void indexValidityCheck (int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+    
+    /*
+     * Expands the size of the list whenever it is at
+     * capacity
+     */
+    private void checkAndGrow () {
+        // Case: big enough to fit another item, so no
+        // need to grow
+        if (size < items.length) {
+            return;
+        }
+        
+        // Case: we're at capacity and need to grow
+        // Step 1: create new, bigger array; we'll
+        // double the size of the old one
+        int[] newItems = new int[items.length * 2];
+        
+        // Step 2: copy the items from the old array
+        for (int i = 0; i < items.length; i++) {
+            newItems[i] = items[i];
+        }
+        
+        // Step 3: update IntList reference
+        items = newItems;
+    }
+    
+    /*
+     * Shifts all elements to the right of the given
+     * index one left
+     */
+    private void shiftLeft (int index) {
+        for (int i = index; i < size-1; i++) {
+            items[i] = items[i+1];
+        }
+    }
+    
+    public static void main(String[] args) {
+    	//hehe
+    }
 }
