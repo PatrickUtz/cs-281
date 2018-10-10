c873c2e4866322e6f73b371cbd6a5967dc11c6e1
Patrick Utz <utzpatrick91@gmail.com>
2018-10-09 20:44:29 -0700

    started HW 2

--- class_examples/src/gcd/GCD.java
+++ class_examples/src/gcd/GCD.java
@@ -8,7 +8,7 @@ public class GCD {
 	 */
 	public static int gcd(int x, int y) {
 		// Consider x to be larger of x and y
-		if (y < x) {
+		if (y > x) {
 			int t = x; 
 			x = y; 
 			y = t;
@@ -18,19 +18,23 @@ public class GCD {
 		if (x % y == 0) {
 			return y;
 		}
+//		
+//		// Dumb case: try all the numbers to see which is 
+//		// the greatest denominator
+//		int currentGreatest = 1;
+//		for (int i = 0; i <= y; i++) {
+//			// Case: found a divisor
+//			if (x % i == 0 && y % i == 0) {
+//				currentGreatest = i;
+//			}
+//		}
+//		return currentGreatest;
+//	}
 		
-		// Dumb case: try all the numbers to see which is 
-		// the greatest denominator
-		int currentGreatest = 1;
-		for (int i = 0; i <= y; i++) {
-			// Case: found a divisor
-			if (x % i == 0 && y % i == 0) {
-				currentGreatest = i;
-			}
-		}
-		return currentGreatest;
+		// Euclid's algorithm:
+		return gcd(y, x % y);
 	}
-	
+		
 	public static void main (String[] args) {
 		System.out.println(gcd(10, 5));
 		System.out.println(gcd(21, 49));
---
+++ homework/src/linked_forneymonegerie/LinkedForneymonegerie.java
@@ -0,0 +1,153 @@
+package linked_forneymonegerie;
+
+import java.util.NoSuchElementException;
+
+public class LinkedForneymonegerie implements LinkedForneymonegerieInterface {
+
+    // Fields
+    // -----------------------------------------------------------
+    private ForneymonType head;
+    private int size, typeSize, modCount;
+    
+    
+    // Constructor
+    // -----------------------------------------------------------
+    LinkedForneymonegerie () {
+        head = null;
+        size = 0;
+        typeSize = 0;
+        modCount = 0;
+    }
+    
+    
+    // Methods
+    // -----------------------------------------------------------
+    public boolean empty () {
+    	return size == 0;
+    }
+    
+    public int size () {
+        return size;
+    }
+    
+    public int typeSize () {
+    	return typeSize;
+    }
+    
+    public boolean collect (String toAdd) {
+        size++;
+        modCount++;
+    	if (head == null) {
+        	head = new ForneymonType(toAdd, 1);
+        	head.prev = null;
+        	head.next = null;
+        	return true; 
+        }
+    	
+    }
+    
+    public boolean release (String toRemove) {
+        throw new UnsupportedOperationException();
+    }
+    
+    public void releaseType (String toNuke) {
+        throw new UnsupportedOperationException();
+    }
+    
+    public int countType (String toCount) {
+        throw new UnsupportedOperationException();
+    }
+    
+    public boolean contains (String toCheck) {
+        throw new UnsupportedOperationException();
+    }
+    
+    public String rarestType () {
+        throw new UnsupportedOperationException();
+    }
+    
+    public LinkedForneymonegerie clone () {
+        throw new UnsupportedOperationException();
+    }
+    
+    public void trade (LinkedForneymonegerie other) {
+        throw new UnsupportedOperationException();
+    }
+    
+    public LinkedForneymonegerie.Iterator getIterator () {
+        throw new UnsupportedOperationException();
+    }
+    
+    
+    // -----------------------------------------------------------
+    // Static methods
+    // -----------------------------------------------------------
+    
+    public static LinkedForneymonegerie diffMon (LinkedForneymonegerie y1, LinkedForneymonegerie y2) {
+        throw new UnsupportedOperationException();
+    }
+    
+    public static boolean sameCollection (LinkedForneymonegerie y1, LinkedForneymonegerie y2) {
+        throw new UnsupportedOperationException();
+    }
+    
+    // Private helper methods
+    // -----------------------------------------------------------
+
+    // TODO: Your helper methods here!
+    
+    
+    // Inner Classes
+    // -----------------------------------------------------------
+    
+    public class Iterator implements LinkedForneymonegerieIteratorInterface {
+        LinkedForneymonegerie owner;
+        ForneymonType current;
+        int itModCount;
+        
+        Iterator (LinkedForneymonegerie y) {
+            // TODO
+        }
+        
+        public boolean hasNext () {
+            throw new UnsupportedOperationException();
+        }
+        
+        public boolean hasPrev () {
+            throw new UnsupportedOperationException();
+        }
+        
+        public boolean isValid () {
+            throw new UnsupportedOperationException();
+        }
+        
+        public String getType () {
+            throw new UnsupportedOperationException();
+        }
+
+        public void next () {
+            throw new UnsupportedOperationException();
+        }
+        
+        public void prev () {
+            throw new UnsupportedOperationException();
+        }
+        
+        public void replaceAll (String toReplaceWith) {
+            throw new UnsupportedOperationException();
+        }
+        
+    }
+    
+    private class ForneymonType {
+        ForneymonType next, prev;
+        String type;
+        int count;
+        
+        ForneymonType (String t, int c) {
+            type = t;
+            count = c;
+        }
+    }
+    
+}
