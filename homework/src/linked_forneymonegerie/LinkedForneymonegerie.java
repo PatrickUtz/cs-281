aebbdac7f1da71178e925f4873ec8bf7a14a62dd
Patrick Utz <utzpatrick91@gmail.com>
2018-10-12 14:13:05 -0700

    update hw 2

--- homework/src/linked_forneymonegerie/LinkedForneymonegerie.java
+++ homework/src/linked_forneymonegerie/LinkedForneymonegerie.java
@@ -35,43 +35,132 @@ public class LinkedForneymonegerie implements LinkedForneymonegerieInterface {
     }
     
     public boolean collect (String toAdd) {
-        size++;
         modCount++;
-    	if (head == null) {
+                
+        if (empty()) {
         	head = new ForneymonType(toAdd, 1);
-        	head.prev = null;
-        	head.next = null;
-        	return true; 
+    		head.prev = null;
+    		head.next = null;
+            size++;
+    		typeSize++;
+    		return true;
         }
-    	
+        
+        size++;
+        ForneymonType typeToAdd = getType(toAdd);
+        if (typeToAdd != null) {
+        	typeToAdd.count++;
+        	return false;
+        } else {
+        	ForneymonType oldEnd = getEnd();
+        	ForneymonType newEnd = new ForneymonType(toAdd, 1);
+        	oldEnd.next = newEnd;
+        	newEnd.prev = oldEnd;
+        	typeSize++;
+        	return true;
+        }
     }
     
     public boolean release (String toRemove) {
-        throw new UnsupportedOperationException();
+    	modCount++;
+    	ForneymonType typeToRelease = getType(toRemove);
+    	if (typeToRelease != null) {
+    		if (typeToRelease.count == 1) {
+    			releaseType(toRemove);
+    		} else {
+    			typeToRelease.count--;
+    			size--;
+    		}
+			return true;
+    	}
+    	return false;
     }
     
     public void releaseType (String toNuke) {
-        throw new UnsupportedOperationException();
+    	modCount++;
+    	ForneymonType typeToNuke = getType(toNuke);
+    	if (typeToNuke != null) {
+    		typeSize--;
+    		size = size - typeToNuke.count;
+    		remove(typeToNuke);
+    	}
+    	return;
     }
     
     public int countType (String toCount) {
-        throw new UnsupportedOperationException();
+    	ForneymonType typeToCount = getType(toCount);
+    	if (typeToCount != null) {
+    		return typeToCount.count;
+    	}
+    	return 0;
     }
     
     public boolean contains (String toCheck) {
-        throw new UnsupportedOperationException();
+        if (getType(toCheck) != null) {
+        	return true;
+        }
+        return false;
     }
     
     public String rarestType () {
-        throw new UnsupportedOperationException();
+    	ForneymonType current = head;
+    	String rarest = null;
+        int rarestCount = current.count;
+        while (current != null) {
+        	if (current.count <= rarestCount) {
+        		rarest = current.type;
+        		rarestCount = current.count;
+        	}
+        	current = current.next;
+        }
+        return rarest;
     }
     
     public LinkedForneymonegerie clone () {
-        throw new UnsupportedOperationException();
+    	LinkedForneymonegerie clone = new LinkedForneymonegerie();
+    	ForneymonType current = head;
+    	ForneymonType cloneCurrent = null;
+    	ForneymonType clonePrev = null;
+    	while (current != null) {
+    		if (clone.head == null) {
+    			clone.head = new ForneymonType(head.type, head.count);
+    			cloneCurrent = clone.head;
+    		} else {
+    			cloneCurrent.next = new ForneymonType(current.type, current.count);
+        		cloneCurrent.next.prev = cloneCurrent; 
+        		cloneCurrent = cloneCurrent.next;
+    		}
+    		current = current.next;
+    	}
+    	clone.modCount = modCount;
+    	clone.size = size;
+    	clone.typeSize = typeSize;
+    	return clone;
     }
     
     public void trade (LinkedForneymonegerie other) {
-        throw new UnsupportedOperationException();
+    	ForneymonType tempHead = head;
+    	int tempSize = size;
+    	int tempTypeSize = typeSize;
+    	int tempModCount = modCount;
+    	
+    	head = other.head;
+    	size = other.size;
+    	typeSize = other.typeSize;
+    	modCount = other.modCount;
+    	
+    	other.head = tempHead;
+    	other.size = tempSize;
+    	other.typeSize = tempTypeSize;
+    	other.modCount = tempModCount;
+    	
+    	modCount++;
+    	other.modCount++;
+    }
+    
+    @Override
+    public String toString() {
+    	throw new UnsupportedOperationException();
     }
     
     public LinkedForneymonegerie.Iterator getIterator () {
@@ -95,6 +184,53 @@ public class LinkedForneymonegerie implements LinkedForneymonegerieInterface {
     // -----------------------------------------------------------
 
     // TODO: Your helper methods here!
+    
+    private ForneymonType getType (String typeToFind) {
+    	if (empty()) {
+    		return null;
+    	}
+    	ForneymonType current = head;
+    	while (current != null) {
+    		if (current.type.equals(typeToFind)) {
+    			return current;
+    		}
+    		current = current.next;
+    	}
+    	return null;
+    }
+    
+    private void remove (ForneymonType toRemove) {
+    	if (toRemove == head) {
+    		if (head.next != null) {
+    			head.next.prev = null;
+    			head = head.next;
+    			return;
+    		} else {
+    			head = null;
+    		}
+    	} else if (isEnd(toRemove)) {
+    		toRemove.prev.next = null;
+    	} else {
+    		toRemove.prev.next = toRemove.next;
+    		toRemove.next.prev = toRemove.prev;
+    	}
+    	return;
+    }
+    
+    private boolean isEnd (ForneymonType toCheck) {
+    	if (toCheck.next == null) {
+    		return true;
+    	}
+    	return false;
+    }
+    
+    private ForneymonType getEnd () {
+    	ForneymonType current = head;
+    	while (!isEnd(current)) {
+    		current = current.next;
+    	}
+    	return current;
+    }
     
     
     // Inner Classes
